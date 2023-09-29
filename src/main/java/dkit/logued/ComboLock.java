4dadfa75f369c6229a70e34f467412b0f96b2ac3
dermo <dermot.logue@dkit.ie>
2023-09-29 15:06:52 +0100

    braces for if added

--- src/main/java/dkit/logued/ComboLock.java
+++ src/main/java/dkit/logued/ComboLock.java
@@ -1,7 +1,6 @@
 package dkit.logued;
 
 public class ComboLock {
-
     // The three correct (secret) combination numbers for this ComboLock
     // are stored in order (Right,Left,Right), and initialized in constructor
     private int secret1;
@@ -9,8 +8,8 @@ public class ComboLock {
     private int secret3;
 
     private ComboLockState comboLockState;  // Enum to store current state of Lock
-                                            // See enum definition in ComboLockState.java
 
+    // See enum definition in ComboLockState.java
     // constructor
     public ComboLock(int num1, int num2, int num3) {
         this.secret1 = num1;    // program the lock with the code to open the lock
@@ -23,21 +22,23 @@ public class ComboLock {
         this.comboLockState = ComboLockState.NONE_CORRECT;
     }
 
-    public void turnRight(int number) {
-        if (comboLockState == ComboLockState.NONE_CORRECT && secret1 == number)
-            comboLockState = ComboLockState.FIRST_CORRECT;
-        else {
 
-            if (comboLockState == ComboLockState.SECOND_CORRECT && secret3 == number)
+    public void turnRight(int number) {
+        if (comboLockState == ComboLockState.NONE_CORRECT && secret1 == number) {
+            comboLockState = ComboLockState.FIRST_CORRECT;
+        } else {
+            if (comboLockState == ComboLockState.SECOND_CORRECT && secret3 == number) {
                 comboLockState = ComboLockState.ALL_CORRECT;
-            else
+            } else {
                 reset();    // if neither of above then, reset() lock to NONE_CORRECT state
+            }
         }
     }
 
     public void turnLeft(int number) {
-        if (comboLockState == ComboLockState.FIRST_CORRECT && secret2 == number)
+        if (comboLockState == ComboLockState.FIRST_CORRECT && secret2 == number) {
             comboLockState = ComboLockState.SECOND_CORRECT;
+        }
         else
             reset();
     }
