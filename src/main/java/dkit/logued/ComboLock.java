29ba0b83f4ebba54542d86ea60b05d7bbfa0c354
logued <logued@dkit.ie>
2025-09-17 14:05:43 +0100

    sdk 22

--- src/main/java/dkit/logued/ComboLock.java
+++ src/main/java/dkit/logued/ComboLock.java
@@ -2,7 +2,7 @@ package dkit.logued;
 
 public class ComboLock {
     // The three correct (secret) combination numbers for this ComboLock
-    // are stored in order (Right,Left,Right), and initialized in constructor
+    // are stored in order (Right,Left,Right), and initialized in the constructor
     private final int secret1;
     private final int secret2;
     private final int secret3;
@@ -10,11 +10,12 @@ public class ComboLock {
     private ComboLockState comboLockState;  // Enum to store current state of Lock
 
     // See enum definition in ComboLockState.java
+
     // constructor
-    public ComboLock(int num1, int num2, int num3) {
-        this.secret1 = num1;    // program the lock with the code to open the lock
-        this.secret2 = num2;
-        this.secret3 = num3;
+    public ComboLock(int digit1, int digit2, int digit3) {
+        this.secret1 = digit1;    // program the lock with the code to open the lock
+        this.secret2 = digit2;
+        this.secret3 = digit3;
         this.comboLockState = ComboLockState.NONE_CORRECT;  // set initial state
     }
 
@@ -30,7 +31,7 @@ public class ComboLock {
             if (comboLockState == ComboLockState.SECOND_CORRECT && secret3 == number) {
                 comboLockState = ComboLockState.ALL_CORRECT;
             } else {
-                reset();    // if neither of above then, reset() lock to NONE_CORRECT state
+                reset();    // if none of the above, then, reset() lock to NONE_CORRECT state
             }
         }
     }
@@ -46,8 +47,8 @@ public class ComboLock {
     public boolean open() {
         if (comboLockState == ComboLockState.ALL_CORRECT)
             return true;
-
-        return false;
+        else
+            return false;
     }
 }
 
